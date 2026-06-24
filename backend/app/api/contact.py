0b7d4ab5a9431aecc3d23167d2a3640d03a0c2f0
bwbeas <bellbuuuwork6@gmail.com>
2026-06-24 19:21:20 +0530

    test

--- backend/app/api/contact.py
+++ backend/app/api/contact.py
@@ -3,58 +3,67 @@ from fastapi import APIRouter
 from app.schemas.contact import ContactMessage
 
 import smtplib
+import os
 
 from email.message import EmailMessage
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL_ADDRESS = os.getenv(
+  "EMAIL_ADDRESS"
+)
+
+EMAIL_PASSWORD = os.getenv(
+  "EMAIL_PASSWORD"
+)
+
 router = APIRouter(
-    prefix="/contact",
-    tags=["Contact"]
+  prefix="/contact",
+  tags=["Contact"]
 )
 
 @router.post("/")
 def send_message(data: ContactMessage):
 
-    email = EmailMessage()
 
-    email["Subject"] = "New Nutriki Message 🌷"
+  email = EmailMessage()
 
-    email["From"] = "bellbuuuwork6@gmail.com"
+  email["Subject"] = "New Nutriki Message 🌷"
 
-    email["To"] = "bellbuuuwork6@gmail.com"
+  email["From"] = EMAIL_ADDRESS
 
-    email.set_content(
+  email["To"] = EMAIL_ADDRESS
+  email.set_content(
 
-        f"""
-Nickname:
-{data.nickname}
+    f"""
+    Nickname:
+    {data.nickname}
 
-Email:
-{data.email}
+    Email:
+    {data.email}
 
-Message:
-{data.message}
-        """
+    Message:
+    {data.message}
+    """
 
+  )
+
+  with smtplib.SMTP_SSL(
+    "smtp.gmail.com",
+    465
+  ) as smtp:
+
+    smtp.login(
+        EMAIL_ADDRESS,
+        EMAIL_PASSWORD
     )
 
-    with smtplib.SMTP_SSL(
-        "smtp.gmail.com",
-        465
-    ) as smtp:
+    smtp.send_message(email)
 
-        smtp.login(
+  return {
+    "message":
+    "sent successfully"
+  }
 
-            "bellbuuuwork6@gmail.com",
-
-            "tmlvcdspqstsicvn"
-
-        )
-
-        smtp.send_message(email)
-
-    return {
-
-        "message":
-        "sent successfully"
-
-    }
